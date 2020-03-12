79dc1824b678358990a900e6d28a204e2764bb7c
Carl Hain <crhain@gmail.com>
2020-03-12 18:47:17 -0500

    Add check for whether player wins or loses when attacking

--- app.js
+++ app.js
@@ -16,10 +16,23 @@ new Vue({
       let min = 3;
       let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
       this.monsterHealth -= damage;
+
+      if (this.monsterHealth <= 0) {
+        alert("You won!");
+        this.gameIsRunning = false;
+        return;
+      }
+
       max = 12;
       min = 5;
       damage = Math.max(Math.floor(Math.random() * max) + 1, min);
       this.playerHealth -= damage;
+
+      if (this.playerHealth <= 0) {
+        alert("You lost!");
+        this.gameIsRunning = false;
+        return;
+      }
     },
     specialAttack() {},
     heal() {},
